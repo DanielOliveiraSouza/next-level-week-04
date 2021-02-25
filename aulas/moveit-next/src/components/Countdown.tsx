c0350e1d1554a8e2ee72652b3ddff742078f65ca
DanielOliveiraSouza <oliveira.daniel109@gmail.com>
2021-02-25 02:20:45 -0300

    adiciona estilos para o relogio em Countdown

--- aulas/moveit-next/src/components/Countdown.tsx
+++ aulas/moveit-next/src/components/Countdown.tsx
@@ -6,7 +6,7 @@ export function CountDown (){
                 <span>2</span>
                 <span>5</span>
             </div>
-
+            <span>:</span>
             <div>
                 <span>0</span>
                 <span>0</span>
