36c19392d0cc3f88b4cd6195289379fe050ac0c3
DanielOliveiraSouza <oliveira.daniel109@gmail.com>
2021-02-25 01:25:07 -0300

    adiciona Estilos para os componentes Profile e Home

--- aulas/moveit-next/src/components/Profile.tsx
+++ aulas/moveit-next/src/components/Profile.tsx
@@ -1,6 +1,8 @@
+import styles from  '../styles/components/Profile.module.css';
+
 export function Profile (){
     return (
-        <div>
+        <div  className={styles.profileContainer}>
             <img src="https://github.com/danieloliveirasouza.png" alt="Daniel Oliveira"/>
             <div>
                <strong>Daniel Oliveira Souza</strong>
--- aulas/moveit-next/src/pages/index.tsx
+++ aulas/moveit-next/src/pages/index.tsx
@@ -1,21 +1,22 @@
 import Head from 'next/head';
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
+import styles from '../styles/components/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <ExperienceBar/>
       
       <section>
-        <div>
+        <div className={styles.section}>
           <Profile/>
         </div>
 
         <div>
 
         </div>
-        
+
       </section>
     </div>
   );
