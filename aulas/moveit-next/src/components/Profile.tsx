8a786d844c7c364ddd0bc34a18af06b79914b2f3
DanielOliveiraSouza <oliveira.daniel109@gmail.com>
2021-02-25 01:10:58 -0300

    adiciona componente Profile.tsx

---
+++ aulas/moveit-next/src/components/Profile.tsx
@@ -0,0 +1,13 @@
+export function Profile (){
+    return (
+        <div>
+            <img src="https://github.com/danieloliveirasouza.png" alt="Daniel Oliveira"/>
+            <div>
+               <strong>Daniel Oliveira Souza</strong>
+                <p>Level 1</p>
+            </div>
+        
+        </div>
+
+    );
+}
