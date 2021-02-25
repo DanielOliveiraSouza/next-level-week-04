3465a3759b09fce43c2c03210b9e7aa5d769d937
DanielOliveiraSouza <oliveira.daniel109@gmail.com>
2021-02-25 02:08:50 -0300

    Adiciona Componente Countdown e  estilizaçao

---
+++ aulas/moveit-next/src/components/Countdown.tsx
@@ -0,0 +1,16 @@
+import styles  from '../styles/components/Countdown.module.css';
+export function CountDown (){
+    return (
+        <div className={styles.countdownContainer}>
+            <div>
+                <span>2</span>
+                <span>5</span>
+            </div>
+
+            <div>
+                <span>0</span>
+                <span>0</span>
+            </div>
+        </div>
+    );
+}
