9562d0f71dda7dc99f54e8fbf843d811b1f5f07a
DanielOliveiraSouza <oliveira.daniel109@gmail.com>
2021-02-26 01:25:07 -0300

    adiciona conteudo e estilizaçao para ChallengeBox

--- aulas/moveit-next/src/pages/index.tsx
+++ aulas/moveit-next/src/pages/index.tsx
@@ -19,6 +19,8 @@ export default function Home() {
           <Profile/>
           <CompletedChallenges/>
           <Countdown/>
+        </div>
+        <div>
           <ChallengeBox/>
         </div>
 
